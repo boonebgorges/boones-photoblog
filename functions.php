a532376f09ba87bf76e3276564b0a16159f78714
Boone Gorges <boonebgorges@gmail.com>
2011-06-05 16:20:48 -0400

    Hashtag conversion

--- functions.php
+++ functions.php
@@ -6,6 +6,9 @@ function bbg_pb_the_title() {
 	// Turn @-mentions into links
 	$title = preg_replace("/[@]+([A-Za-z0-9-_]+)/", "<a href=\"http://twitter.com/\\1\" target=\"_blank\">\\0</a>", $title );
 
+	// Turn hashtags into links
+	$title = preg_replace("/ [#]+([A-Za-z0-9-_]+)/", " <a href=\"http://twitter.com/search?q=%23\\1\" target=\"_blank\">\\0</a>", $title );
+
 	echo $title;
 }
 
