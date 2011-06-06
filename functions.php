b0358dcb65f1593dc76808100f0214a7735fd3fe
Boone Gorges <boonebgorges@gmail.com>
2011-06-06 15:39:30 -0400

    Filter juggling

--- functions.php
+++ functions.php
@@ -3,13 +3,20 @@
 function bbg_pb_the_title() {
 	$title = get_the_title();
 
-	// Turn @-mentions into links
-	$title = preg_replace("/[@]+([A-Za-z0-9-_]+)/", "<a href=\"http://twitter.com/\\1\" target=\"_blank\">\\0</a>", $title );
-
-	// Turn hashtags into links
-	$title = preg_replace("/ [#]+([A-Za-z0-9-_]+)/", " <a href=\"http://twitter.com/search?q=%23\\1\" target=\"_blank\">\\0</a>", $title );
+	$title = bbg_pb_twitterize( $title );
 
 	echo $title;
 }
+
+function bbg_pb_twitterize( $content ) {
+	// Turn @-mentions into links
+	$content = preg_replace("/[@]+([A-Za-z0-9-_]+)/", "<a href=\"http://twitter.com/\\1\" target=\"_blank\">\\0</a>", $content );
+
+	// Turn hashtags into links
+	$content = preg_replace("/ [#]+([A-Za-z0-9-_]+)/", " <a href=\"http://twitter.com/search?q=%23\\1\" target=\"_blank\">\\0</a>", $content );
+	
+	return $content;
+}
+add_filter( 'the_content', 'bbg_pb_twitterize' );
 
 ?>
